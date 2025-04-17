a72703614ea29f4a9ac69229952d38ac7d22d29f
Andrey-Alohin <andriealohin2020@outlook.com>
2025-04-17 22:11:34 +0300

    Add new component FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,28 +1,12 @@
-import clsx from "clsx";
 import css from "./FriendList.module.css";
+import FriendListItem from "../FriendListItem/FriendListItem";
 export default function FriendList({ friends }) {
   return (
     <ul className={css.friendList}>
       {friends.map(({ avatar, name, isOnline, id }) => {
         return (
           <li key={id} className={css.friendItem}>
-            <div>
-              <img
-                className={css.friendImg}
-                src={avatar}
-                alt="Avatar"
-                width="48"
-              />
-              <p className={css.friendName}>{name}</p>
-              <p
-                className={clsx(
-                  css.friendStatus,
-                  isOnline ? css.online : css.offline
-                )}
-              >
-                {isOnline ? "Online" : "Offline"}
-              </p>
-            </div>
+            <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
           </li>
         );
       })}
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+import css from "./FriendListItem.module.css";
+export default function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <div>
+      <img className={css.friendImg} src={avatar} alt="Avatar" width="48" />
+      <p className={css.friendName}>{name}</p>
+      <p
+        className={clsx(css.friendStatus, isOnline ? css.online : css.offline)}
+      >
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+}
